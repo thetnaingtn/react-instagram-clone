4057963fabc3ff9dc412cb9ff27f15f365a2c359
Thet Naing Tun <thetnaingtun@bindez.com>
2021-08-07 21:37:24 +0630

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Login = lazy(() => import("./pages/login"));
+
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Router>
+    </Suspense>
+  );
 }
 
 export default App;
