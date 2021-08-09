1db21f4243615a251bc25c4fe55453e0706655f4
Thet Naing Tun <thetnaingtun@bindez.com>
2021-08-09 19:53:58 +0630

    add useAuthListener hook and user context

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import * as Routes from "./constants/routes";
+import useAuthListener from "./hooks/user-auth-listener";
+import UserContext from "./constants/user";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
 const NotFound = lazy(() => import("./pages/not-found"));
 
 function App() {
+  const { user } = useAuthListener();
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <Router>
-        <Switch>
-          <Route path={Routes.LOGIN} component={Login} />
-          <Route path={Routes.SIGN_UP} component={SignUp} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
-    </Suspense>
+    <UserContext.Provider value={{ user }}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Router>
+          <Switch>
+            <Route path={Routes.LOGIN} component={Login} />
+            <Route path={Routes.SIGN_UP} component={SignUp} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </Suspense>
+    </UserContext.Provider>
   );
 }
 
