04ac3d2283736c5d7bff4de39ade1df4bc883480
Thet Naing Tun <thetnaingtun@bindez.com>
2021-08-09 15:26:13 +0630

    dev not found page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import * as Routes from "./constants/routes";
 
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
+const NotFound = lazy(() => import("./pages/not-found"));
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path={Routes.LOGIN} component={Login} />
           <Route path={Routes.SIGN_UP} component={SignUp} />
+          <Route component={NotFound} />
         </Switch>
       </Router>
     </Suspense>
