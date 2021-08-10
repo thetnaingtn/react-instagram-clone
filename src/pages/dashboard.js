24c4a1a0c3234966dbba84720762f9f59bd074a3
Thet Naing Tun <thetnaingtun@bindez.com>
2021-08-10 11:20:25 +0630

    add title for dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,14 +1,19 @@
+import { useEffect } from "react";
+
 import Header from "../components/header";
 import Timeline from "../components/timeline";
 import Sidebar from "../components/sidebar";
 
 export default function Dashboard() {
+  useEffect(() => {
+    document.title = "Instagram";
+  }, []);
   return (
     <div className="bg-gray-background">
       <Header />
-      <div className="grid">
-        <Sidebar />
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
         <Timeline />
+        <Sidebar />
       </div>
     </div>
   );
